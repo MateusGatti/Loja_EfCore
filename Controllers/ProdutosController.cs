d4028cc5d6a8b946348c443d6415f03f6646b526
Mateus Mello <mt.gatti@hotmail.com>
2020-09-09 19:23:04 -0300

    Aula 07 finalizada

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EfCore.Domains;
+using EfCore.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ProdutoRepository _produtoRepository;
+
+        public ProdutosController()
+        {
+            _produtoRepository = new ProdutoRepository();
+        }
+        [HttpGet]
+        public List<Produto> Get()
+        {
+            return _produtoRepository.Listar();
+        }
+
+        // GET api/<RacaController>/5
+        [HttpGet("{id}")]
+        public Produto Get(Guid id)
+        {
+            return _produtoRepository.BuscarPorId(id);
+        }
+
+        // POST api/<AlunoController>
+        [HttpPost]
+        public void Post(Produto produto)
+        {
+             _produtoRepository.Adicionar(produto);
+        }
+
+        // PUT api/<AlunoController>/5
+        [HttpPut("{id}")]
+        public void Put(Guid id, Produto produto)
+        {
+            _produtoRepository.Editar(produto);
+        }
+
+        // DELETE api/<AlunoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            _produtoRepository.Remover(id);
+        }
+    }
+}
---
+++ Repositories/ProdutoRepository.cs
@@ -0,0 +1,149 @@
+﻿using EfCore.Domains;
+using EfCore.Interfaces;
+using EFCore.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCore.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly PedidoContext _ctx;
+        public ProdutoRepository()
+        {
+            _ctx = new PedidoContext();
+        }
+        
+        /// <summary>
+        /// Adiciona um produto
+        /// </summary>
+        /// <param name="produto">objeto produto</param>
+        public void Adicionar(Produto produto)
+        {
+            try
+            {
+                //Adiciona um produto ao dbset do contexto
+                _ctx.Produtos.Add(produto);
+
+                //Salva as alteracoes no contexto
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Busca um produto pelo id
+        /// </summary>
+        /// <param name="id">Id do produto</param>
+        /// <returns>Produto buscado</returns>
+        public Produto BuscarPorId(Guid id)
+        {
+            try
+            {
+                return _ctx.Produtos.Find(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Busca um produto pelo nome
+        /// </summary>
+        /// <param name="nome">Nome do produto</param>
+        /// <returns>Produto buscado</returns>
+        public List<Produto> BuscarPorNome(string nome)
+        {
+            try
+            {
+                return _ctx.Produtos.Where(c => c.Nome.Contains(nome)).ToList();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Edita um produto
+        /// </summary>
+        /// <param name="produto">Produto que sera editado</param>
+        public void Editar(Produto produto)
+        {
+            try
+            {
+                //Busca o produto por id
+                Produto produtoTemp = BuscarPorId(produto.Id);
+
+                // Verifica se o produto existe
+                // Caso não exista cria uma mensagem
+                if (produtoTemp == null)
+                    throw new Exception("Produto não encontrado");
+
+                //Caso exista, fara a alteração
+                produtoTemp.Nome = produto.Nome;
+                produtoTemp.Preco = produto.Preco;
+
+                _ctx.Produtos.Update(produtoTemp);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Lista os produtos cadastrados
+        /// </summary>
+        /// <returns>Lista de produtos</returns>
+        public List<Produto> Listar()
+        {
+            try
+            {
+                return _ctx.Produtos.ToList();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Remove um produto
+        /// </summary>
+        /// <param name="id">Id do produto</param>
+        public void Remover(Guid id)
+        {
+            try
+            {
+                //Busca o produto por id
+                Produto produtoTemp = BuscarPorId(id);
+
+                // Verifica se o produto existe
+                // Caso não exista cria uma mensagem
+                if (produtoTemp == null)
+                    throw new Exception("Produto não encontrado");
+
+                _ctx.Produtos.Remove(produtoTemp);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
